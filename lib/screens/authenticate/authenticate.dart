956c4a33fbe31511f89879e18a17ad434d437646
Saqib S <msaqibsaeed4@gmail.com>
2020-02-27 14:33:20 +0500

    AnonymousSignInCompleted

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,4 +1,6 @@
+import 'package:brewcrew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
+
 
 class Authenticate extends StatefulWidget {
   @override
@@ -9,7 +11,7 @@ class _AuthenticateState extends State<Authenticate> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('Authenticate'),
+      child: SignIn(),
     );
   }
 }
