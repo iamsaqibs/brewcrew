e3576a921cedc5acd6d49d8950ebb48510a1c854
Saqib S <msaqibsaeed4@gmail.com>
2020-02-29 14:48:04 +0500

    addedUserModel

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,13 +1,19 @@
+import 'package:brewcrew/models/user.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
+
+  User _firebaseToUser(FirebaseUser user){
+    return user != null ? User(uid: user.uid) : null;
+  }
+
   Future signInAnon() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _firebaseToUser(user);
     } catch (e) {
       print(e.toString());
       return null;
